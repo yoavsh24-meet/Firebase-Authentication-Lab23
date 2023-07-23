7256f8b68d25dd2d9095536c2fb2326e7e3c723b
yoavsh24-meet <yoavisnotashuster@gmail.com>
2023-07-23 16:24:13 +0300

    womp womp

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -5,7 +5,15 @@ import pyrebase
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
+config = {
+  "apiKey": "AIzaSyDczgJfyL9axKQ2-nbeMDPdeie4aiqWW9Y",
+  "authDomain": "hungerforflesh-5b423.firebaseapp.com",
+  "projectId": "hungerforflesh-5b423",
+  "storageBucket": "hungerforflesh-5b423.appspot.com",
+  "messagingSenderId": "647572238228",
+  "appId": "1:647572238228:web:49987a13db03d38751e981","databaseURL": ""
 
+};
 @app.route('/', methods=['GET', 'POST'])
 def signin():
     return render_template("signin.html")
@@ -19,6 +27,10 @@ def signup():
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
     return render_template("add_tweet.html")
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 
 if __name__ == '__main__':
